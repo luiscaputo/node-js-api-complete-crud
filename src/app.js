8f03d859ee64a5f2046381abeb363c395e043d73
luiscaputo <luiscaputo15@gmail.com>
2021-06-12 08:44:34 +0100

    :heavy_check_mark: route-create-done

--- src/app.js
+++ src/app.js
@@ -3,24 +3,28 @@ const express = require('express')
 const { sucrase } = require('sucrase')
 const db = require('../models/database/database')
 const notice = require('../models/notice')
+
 const app = express()
 
-app.use(express(json))
-app.use(express(sucrase))
+app.use(express.json())
     //routes
     app.get('/', (req, res) => {
         return res.json({sucesso: true, message: 'i am running'})
     })
 
-    app.post('/create-notice', async (req, res) => {
-        const data = req.body
+    app.post('/create-notice', async (require, response) => {
+        const data = require.body
             const save = await notice.create(data)
         .then(function(){
-            return res.json({sucesso: true, data})
+            return response.json({sucesso: true, data})
         })
         .catch(function(err){
-            return res.json({sucesso: false, err})
+            return response.json({sucesso: false, err})
         })
+    });
+    app.post('/create', async (req, res) => {
+        const data = req.body
+        return res.json({data})
     })
 app.listen(8080, () => {
     console.log('Server Running')
